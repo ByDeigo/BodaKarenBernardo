81bd620285f07e0c574770a1c0810148aab1e531
ByDeigo <diegonb705@gmail.com>
2025-03-04 18:20:18 -0600

    Cambios

--- KB-Boda/src/App.jsx
+++ KB-Boda/src/App.jsx
@@ -8,14 +8,14 @@ function App() {
     <div className="flex flex-col gap-2">
       <img
         src="/images/vecteezy_botanic-illustration_36647509.svg"
-        className="absolute absolute-img w-[120px] mx-auto md:1/2 opacity-45 -left-12 top-30 object-cover"
+        className="absolute absolute-img w-[120px] md:w-[150px] opacity-45 -left-12 top-30 object-cover"
       />
 
       <main className="box-border w-full flex-wrap flex justify-center items-center sm:flex-row gap-2 sm:gap-2.5 relative p-4 overflow-hidden min-h-[100vh] ">
         <div className="w-1/2 max-w-[300px] h-full box-border ">
           <img
             src="/images/Logo_de_Moda_con_Iniciales_Caligráfico_Elegante_Rosa_y_Negro_1_-removebg-preview.png"
-            className="object-cover rounded-md min-w-[300px]"
+            className="object-cover rounded-md min-w-1/2"
           />
           <p className="text-base sm:text-lg md:text-xl text-center mb-3.5 ">
             "Pero, además de todo esto, vístanse de amor, porque es un lazo de unión perfecto.":Colosenses 3:14
